6090765b84cb3a432aed3c7d34d3cc84db6640db
naoto_miyakawa <nullpoir0805@gmail.com>
2022-01-26 21:43:31 +0900

    設定ファイル追記

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,26 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+    config.autoload_paths += Dir["#{config.root}/lib"]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.template_engine :slim
+      g.test_framework :rspec,
+                       fixture: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true,
+                       system_specs: false
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.helper false
+      g.assets false
+      g.decorator false
+    end
   end
 end
